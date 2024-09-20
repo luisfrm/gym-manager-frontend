82b109f1eb3754dfd274856c186bc18cce0bd80a
Luis Rivas <luisfrm_@outlook.com>
2024-09-20 19:17:51 -0400

    added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@pages': '/src/pages',
+      '@components': '/src/components',
+      '@': '/src',
+    },
+  },
 })
