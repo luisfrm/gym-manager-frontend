49da9b6dec2583d315c7be6fd4ff444aafb7a2e0
Luis <luisfrm_@outlook.com>
2024-09-22 06:35:01 -0400

    added homepage page

--- src/config/RoutesConfig.tsx
+++ src/config/RoutesConfig.tsx
@@ -3,11 +3,13 @@ import Login from "@/pages/Login";
 import Dashboard from "@pages/Dashboard";
 import ProtectedRouted from "@pages/ProtectedRouted";
 import Register from "@pages/Register";
+import Home from "@pages/Home";
 
 const RoutesConfig = () => {
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={
